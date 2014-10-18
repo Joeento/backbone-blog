5625c0ff1f1f5c760bbb56706e448fe40844106e
Eric Kudler <joeento@gmail.com>
2014-10-18 15:37:11 -0400

    Updated view to display data pulled by collection

--- public/js/views.js
+++ public/js/views.js
@@ -4,8 +4,20 @@ HomeView = Backbone.View.extend({
     },
     render: function(){
         // Compile the template using underscore
-        var template = _.template( tpl.get('home'), {} );
+        var template = _.template(tpl.get('home'), {} );
         // Load the compiled HTML into the Backbone "el"
-        this.$el.html( template );
+        this.$el.html(template);
+        bpc = new BlogPostCollection();
+        bpc.fetch({
+            success: function(data) {
+                for (var i=0;i<data.models.length;i++) {
+                    var li = $('<li/>');
+                    li.append(data.models[i].attributes.title)
+                    $('#post-listing').append(li);
+                }
+                
+                
+            }
+        });
     }
 });
