2bf202dde383e891c39d748526ea35b727c8b184
Eric Kudler <joeento@gmail.com>
2014-10-19 16:49:33 -0400

    Collections are now customizable to query by post or tags

--- public/js/collections.js
+++ public/js/collections.js
@@ -1,4 +1,13 @@
 BlogPostCollection = Backbone.Collection.extend({
 	model:BlogPost,
-	url:"/api/posts"
+	initialize: function(options) {
+		console.log(options);
+		options = options || {}
+		console.log(options);
+		this.type = options.type || 'posts'
+		this.value = options.value || '' 
+	},
+	url: function() {
+		return '/api/' + this.type + '/' + this.value
+	}
 });
